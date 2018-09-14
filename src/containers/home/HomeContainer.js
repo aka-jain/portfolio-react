f14f3b4e3d47e9a710b17414c1842a3296db8e73
Akash Jain <akashjain993@gmail.com>
2018-09-14 16:42:15 +0530

    Added circles

--- src/containers/home/HomeContainer.js
+++ src/containers/home/HomeContainer.js
@@ -3,18 +3,73 @@ import React, { Component, Fragment } from 'react';
 import 'styles/home.scss'
 
 class HomeContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.renderCicle = this.renderCicle.bind(this);
+    this.state = {};
+  }
+
+  renderCicle(element, index){
+    console.log(element)
+    return (
+      <div key={index} className={`circle circle-${element.classIndex}`}></div>
+    )
+  }
   render() {
+
+    let circles = [
+      {
+        index: 1,
+        classIndex: 'left-one' 
+      },
+      {
+        index: 2,
+        classIndex: 'left-two' 
+      },
+      {
+        index: 3,
+        classIndex: 'left-three' 
+      },
+      {
+        index: 4,
+        classIndex: 'left-four' 
+      },
+      {
+        index: 5,
+        classIndex: 'left-five' 
+      },
+      {
+        index: 2,
+        classIndex: 'left-two' 
+      },
+      {
+        index: 1,
+        classIndex: 'left-one' 
+      },
+      {
+        index: 2,
+        classIndex: 'left-two' 
+      }
+    ]
+    
     return (
       <div className="container-left-right center">
+      
         <div className="row">
           <div className="col-xs-12 col-sm-4">
             <div className="flex-center skill-home font-geo">
+
+              <div className="circle-fill">
+                {circles.map(this.renderCicle)}
+              </div>
+
               <h2>Design</h2>
             </div>
             
           </div>
            <div className="col-xs-12 col-sm-4">
-            <ul class="logo-list">
+            <ul className="logo-list">
               <li>
                 A
               </li>
@@ -23,7 +78,12 @@ class HomeContainer extends Component {
               </li>
               <li className="logo-img">
               <div className="ring-sm">
-                
+                <div className="ring-inner-sm">
+                </div>
+              </div>
+              <div className="ring-bg">
+                <div className="ring-inner-bg">
+                </div>
               </div>
                 <img src="/assets/images/logo.png" alt=""/>
               </li>
